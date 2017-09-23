c55d028e2782c88d84f1cc17e3acfddb308fff86
Kevin Martensson <kevinmartensson@gmail.com>
2017-09-23 20:24:45 +0200

    `Object.values` => `Object.keys`

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 const os = require('os');
 
-const getIp = family => Object.values(os.networkInterfaces()).reduce((arr, interfce) => arr.concat(
-	Object.values(interfce)
-		.filter(x => x.family === family && !x.internal)
-		.map(x => x.address)
-), []);
+const getIp = family => {
+	const interfaces = os.networkInterfaces();
+
+	return Object.keys(interfaces).reduce((arr, x) => {
+		const interfce = interfaces[x];
+
+		return arr.concat(Object.keys(interfce)
+			.filter(x => interfce[x].family === family && !interfce[x].internal)
+			.map(x => interfce[x].address));
+	}, []);
+};
 
 module.exports = () => getIp('IPv4');
 module.exports.v6 = () => getIp('IPv6');
